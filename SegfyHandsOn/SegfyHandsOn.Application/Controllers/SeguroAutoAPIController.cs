0bc6a379f7119fc75a60d691fcd673777596afea
esdras rafael <esdrasrocha1122@gmail.com>
2019-06-15 00:19:00 -0300

    Criando controller SeguroAutoAPI

---
+++ SegfyHandsOn/SegfyHandsOn.Application/Controllers/SeguroAutoAPIController.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SegfyHandsOn.Domain.Entities;
+using SegfyHandsOn.Service.Validators;
+
+namespace SegfyHandsOn.Application.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Usuario")]
+    public class SeguroAutoAPIController : Controller
+    {
+        private SeguroAutoService<SeguroAuto> service = new BaseService<SeguroAuto>();
+
+        public IActionResult Post([FromBody] SeguroAuto item)
+        {
+            try
+            {
+                service.Post<SeguroAutoValidator>(item);
+
+                return new ObjectResult(item.Id);
+            }
+            catch (ArgumentNullException ex)
+            {
+                return NotFound(ex);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex);
+            }
+        }
+
+        public IActionResult Put([FromBody] SeguroAuto item)
+        {
+            try
+            {
+                service.Put<SeguroAutoValidator>(item);
+
+                return new ObjectResult(item);
+            }
+            catch (ArgumentNullException ex)
+            {
+                return NotFound(ex);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex);
+            }
+        }
+
+        public IActionResult Delete(int id)
+        {
+            try
+            {
+                service.Delete(id);
+
+                return new NoContentResult();
+            }
+            catch (ArgumentException ex)
+            {
+                return NotFound(ex);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex);
+            }
+        }
+
+        public IActionResult Get()
+        {
+            try
+            {
+                return new ObjectResult(service.Get());
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex);
+            }
+        }
+
+        public IActionResult Get(int id)
+        {
+            try
+            {
+                return new ObjectResult(service.Get(id));
+            }
+            catch (ArgumentException ex)
+            {
+                return NotFound(ex);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex);
+            }
+        }
+    }
+}
