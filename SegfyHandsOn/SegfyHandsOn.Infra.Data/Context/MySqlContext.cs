2487d285b2a907332fb5a563fda24d6bf9e2ab6e
esdras rafael <esdrasrocha1122@gmail.com>
2019-06-14 22:36:01 -0300

    Adicionado Classe de contexto: Context Adicionado Classe de mapping: SeguroAutoMap

---
+++ SegfyHandsOn/SegfyHandsOn.Infra.Data/Context/MySqlContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SegfyHandsOn.Domain.Entities;
+using SegfyHandsOn.Infra.Data.Mapping;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SegfyHandsOn.Infra.Data.Context
+{
+    public class MySqlContext : DbContext
+    {
+        public DbSet<SeguroAuto> User { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+                optionsBuilder.UseMySql("Server=[SERVIDOR];Port=[PORTA];Database=modelo;Uid=[USUARIO];Pwd=[SENHA]");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<SeguroAuto>(new SeguroAutoMap().Configure);
+        }
+    }
+}
---
+++ SegfyHandsOn/SegfyHandsOn.Infra.Data/Mapping/SeguroAutoMap.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SegfyHandsOn.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SegfyHandsOn.Infra.Data.Mapping
+{
+    public class SeguroAutoMap : IEntityTypeConfiguration<SeguroAuto>
+    {
+        public void Configure(EntityTypeBuilder<SeguroAuto> builder)
+        {
+            builder.ToTable("SeguroAuto");
+
+            builder.HasKey(c => c.Id);
+
+            builder.Property(c => c.NumeroApolice)
+                .IsRequired()
+                .HasColumnName("NumeroApolice");
+
+            builder.Property(c => c.CPF)
+                .IsRequired()
+                .HasColumnName("Cpf");
+
+            builder.Property(c => c.PlacaVeiculo)
+                .IsRequired()
+                .HasColumnName("PlacaVeiculo");
+
+            builder.Property(c => c.ValorPremio)
+                .IsRequired()
+                .HasColumnName("ValorPremio");
+
+        }
+    }
+}
