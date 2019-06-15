8203b62f8810cb1f5206f8f85ab0a33ed1dfd6d1
esdras rafael <esdras_nike@hotmail.com>
2019-06-15 18:04:52 -0300

    Em um novo projeto. Adicionado Models e Context. Migration usando SqlServer realizada com sucesso.

---
+++ SegfyHandsOn_v2/WebAPI/Migrations/20190615205259_CriandoBanco.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebAPI.Migrations
+{
+    public partial class CriandoBanco : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "SegurosAuto",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    NumeroApolice = table.Column<string>(type: "nchar(100)", nullable: false),
+                    CPF = table.Column<string>(type: "nchar(11)", nullable: false),
+                    PlacaVeiculo = table.Column<string>(type: "nchar(7)", nullable: false),
+                    ValorPremio = table.Column<float>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SegurosAuto", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "SegurosAuto");
+        }
+    }
+}
