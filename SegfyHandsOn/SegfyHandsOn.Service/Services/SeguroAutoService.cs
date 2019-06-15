a26406799fecbdee0694110e2fc96483e249393b
esdras rafael <esdrasrocha1122@gmail.com>
2019-06-14 23:47:13 -0300

    Criando classe Service: SeguroAutoService

---
+++ SegfyHandsOn/SegfyHandsOn.Service/Services/SeguroAutoService.cs
@@ -0,0 +1,12 @@
+﻿using SegfyHandsOn.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SegfyHandsOn.Service.Services
+{
+    public class SeguroAutoService : BaseServiceIService<SeguroAuto>
+    {
+
+    }
+}
