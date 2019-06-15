42a6666dda743ba5eac8ec3556e9b362d78c08ff
esdras rafael <esdras_nike@hotmail.com>
2019-06-15 18:08:39 -0300

    Adicionando controllerAPI SeguroAuto

---
+++ SegfyHandsOn_v2/WebAPI/Controllers/SeguroAutoController.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebAPI.Models;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SeguroAutoController : ControllerBase
+    {
+        private readonly Context _context;
+
+        public SeguroAutoController(Context context)
+        {
+            _context = context;
+        }
+
+        // GET: api/SeguroAuto
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<SeguroAuto>>> GetSegurosAuto()
+        {
+            return await _context.SegurosAuto.ToListAsync();
+        }
+
+        // GET: api/SeguroAuto/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<SeguroAuto>> GetSeguroAuto(int id)
+        {
+            var seguroAuto = await _context.SegurosAuto.FindAsync(id);
+
+            if (seguroAuto == null)
+            {
+                return NotFound();
+            }
+
+            return seguroAuto;
+        }
+
+        // PUT: api/SeguroAuto/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutSeguroAuto(int id, SeguroAuto seguroAuto)
+        {
+            if (id != seguroAuto.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(seguroAuto).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!SeguroAutoExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/SeguroAuto
+        [HttpPost]
+        public async Task<ActionResult<SeguroAuto>> PostSeguroAuto(SeguroAuto seguroAuto)
+        {
+            _context.SegurosAuto.Add(seguroAuto);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetSeguroAuto", new { id = seguroAuto.Id }, seguroAuto);
+        }
+
+        // DELETE: api/SeguroAuto/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<SeguroAuto>> DeleteSeguroAuto(int id)
+        {
+            var seguroAuto = await _context.SegurosAuto.FindAsync(id);
+            if (seguroAuto == null)
+            {
+                return NotFound();
+            }
+
+            _context.SegurosAuto.Remove(seguroAuto);
+            await _context.SaveChangesAsync();
+
+            return seguroAuto;
+        }
+
+        private bool SeguroAutoExists(int id)
+        {
+            return _context.SegurosAuto.Any(e => e.Id == id);
+        }
+    }
+}
